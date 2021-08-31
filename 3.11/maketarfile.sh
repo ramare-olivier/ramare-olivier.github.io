c0fea98739f66547a968c04d3dca321522264fad
ramare-olivier <89833526+ramare-olivier@users.noreply.github.com>
2021-08-31 09:47:12 +0200

    Delete 3.11 directory

--- 3.11/maketarfile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# To be used in the same directory as all the files.
-cp README pariemacs-3.11.txt
-
-tar cvf pariemacs-3.11.tar README CHANGES COPYING Makefile pari.el pari-completion.el pari-conf.el.in pariemacs-3.11.txt pari-fontification.el pari-help.el pari-history.el pari-messages.el sli-tools.el maketarfile.sh pari.cfg-default
-
-gzip -9 pariemacs-3.11.tar
-exit 0

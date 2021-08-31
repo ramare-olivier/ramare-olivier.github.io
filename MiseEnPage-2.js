776b85979220c885ab0fdef6909a3f17ffeeff64
ramare-olivier <89833526+ramare-olivier@users.noreply.github.com>
2021-08-31 09:20:28 +0200

    Add files via upload

---
+++ MiseEnPage-2.js
@@ -0,0 +1,49 @@
+
+
+
+$(document).ready(function(){
+    $('li.paper .Abstract').click(function() {
+        var text = $(this).parent().siblings('.resumecontent').eq(0);
+        if(text.is(':hidden')){
+            text.slideDown('500');
+            $(this).parent().siblings('.bidule').eq(0).html('-');
+        }
+        else {
+            text.slideUp('300');
+            $(this).parent().siblings('.bidule').eq(0).html('+');
+        }
+    });
+    $('li.paper .bidule').click(function() {
+        var text = $(this).siblings('.resumecontent').eq(0);
+        if(text.is(':hidden')){
+            text.slideDown('500');
+            $(this).html('-');
+        }
+        else {
+            text.slideUp('300');
+            $(this).html('+');
+        }
+    });
+    $('li.paper .resumecontent').click(function() {
+        $(this).slideUp('300');
+        $(this).siblings('.bidule').eq(0).html('+');
+    });
+});
+
+$(document).ready(function(){
+    $('#TMEEMTMenu li .bidule').click(function() {
+        var text = $(this).siblings('.resumecontent').eq(0);
+        if(text.is(':hidden')){
+            text.slideDown('500');
+            $(this).html('-');
+        }
+        else {
+            text.slideUp('300');
+            $(this).html('+');
+        }
+    });
+    $('#TMEEMTMenu li .resumecontent').click(function() {
+        $(this).slideUp('300');
+        $(this).siblings('.bidule').eq(0).html('+');
+    });
+});

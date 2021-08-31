05d26df51642e1bee99345ee43975e686aceba05
ramare-olivier <89833526+ramare-olivier@users.noreply.github.com>
2021-08-31 09:47:26 +0200

    Delete 3.07 directory

--- 3.07/maketarfile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-tar cvf pariemacs-3.07.tar README CHANGES COPYING Makefile pari.el pari-completion.el pari-conf.el.in pariemacs-3.07.txt pari-fontification.el pari-help.el pari-history.el pari-messages.el sli-tools.el maketarfile.sh
-
-gzip -9 pariemacs-3.07.tar
-exit 0

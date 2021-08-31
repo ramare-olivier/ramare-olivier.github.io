1b6d8686ef2c89287bbd1067fd0b24682892de51
ramare-olivier <89833526+ramare-olivier@users.noreply.github.com>
2021-08-31 09:48:28 +0200

    Delete 3.10 directory

--- 3.10/maketarfile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# To be used in the same directory as all the files.
-cp README pariemacs-3.10.txt
-
-tar cvf pariemacs-3.10.tar README CHANGES COPYING Makefile pari.el pari-completion.el pari-conf.el.in pariemacs-3.10.txt pari-fontification.el pari-help.el pari-history.el pari-messages.el sli-tools.el maketarfile.sh pari.cfg-default
-
-gzip -9 pariemacs-3.10.tar
-exit 0

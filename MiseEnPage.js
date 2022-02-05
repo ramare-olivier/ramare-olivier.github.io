0153f71edb769dfe67a761d4143dfc108077a9f2
Olivier Ramare <Olivier.Ramare@math.cnrs.fr>
2022-02-05 10:51:28 +0100

    Software type modified

--- MiseEnPage.js
+++ MiseEnPage.js
@@ -240,7 +240,8 @@ function Preprint(Titre, AdditionalAuthor, hreflocal, year, nbpages, note) {
 }
 
 function Software(Titre, AdditionalAuthor, hreflocal, year, nblines, note) {
-    var newHTML ='<img src="../trucs/cat-gets-mouse.png" height="50px">';
+    var newHTML ='<img src="../trucs/cat-gets-mouse.png" 
+                   class="centeredimage" height="50px">';
     
     newHTML += '<span class="titrearticle">' + Titre + '</span>';
     newHTML += ' <span class="ou">(' + year + ')</span>';

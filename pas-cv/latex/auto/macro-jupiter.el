c66bb5a665035607c3d9912d2bb3aac62095788c
ramare-olivier <Olivier.Ramare@math.cnrs.fr>
2024-02-13 13:50:38 +0100

    cv

---
+++ pas-cv/latex/auto/macro-jupiter.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "macro-jupiter"
+ (lambda ()
+   (TeX-add-symbols
+    "fond"
+    "infoLeft"
+    "infoRight"
+    "title"
+    "margins"
+    "clearpage"
+    "clearmargins"))
+ :latex)
+
